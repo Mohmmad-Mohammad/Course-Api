6f8acd662bc2c9e99d1d097bd61945f304c0573f
محمد محمد <softech.mohammadthedark333@gmail.com>
2022-09-03 13:03:19 +0300

    created-PostResource

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -3,14 +3,26 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Resources\PostResource;
 use App\Models\Post;
 use Illuminate\Http\Request;
 
 class PostController extends Controller
 {
+    use ApiResponseTrait;
+
     public function index(){
-        $posts = Post::get();
-        $msg = ["ok"];
-        return response($posts,200,$msg);
+        $posts = PostResource::collection(Post::get());
+        return $this->api($posts,null,200);
     }
+
+    public function show($id)
+    {
+        $post = new PostResource(Post::find($id));
+        if($post){
+            return $this->api($post,null,200);
+        }
+        return $this->api(null,"Not Found",404);
+    }
+
 }
